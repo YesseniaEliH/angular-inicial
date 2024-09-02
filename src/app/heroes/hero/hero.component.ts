a4cf63a7bd91ab328c587a8e38e1bc318e8cc614
Yessenia Eliana Huaman <yesel1601@hotmail.com>
2024-09-02 00:12:45 -0500

    Angular conceptos iniciales

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+export class HeroComponent {
+ public name: string = 'Ironman';
+ public age: number = 45;
+
+ get capitalizedName(): string {
+  return this.name.toUpperCase();
+ }
+
+ getHeroDescription(): string {
+  return `${ this.name } - ${ this.age }`;
+ }
+
+ changeHero(): void {
+  this.name = "Spiderman";
+ };
+
+ changeAge(): void {
+  this.age = 15;
+ };
+
+
+ reset(): void {
+  this.name = "Ironman";
+  this.age = 45;
+
+  document.querySelector('h1')!.innerHTML = '<h1>Desde angular</h1>'
+ };
+}
